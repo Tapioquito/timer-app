d016e033629a5d7844f69bbafd66b8620befb77d
Caio Porto <caioportoramos@gmail.com>
2023-09-19 17:25:42 -0300

    feat: ajusted js methods for the start and pause buttons

--- src/classes/timer.js
+++ src/classes/timer.js
@@ -3,11 +3,30 @@ class Timer {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-    this.startButton.addEventListener("click", this.start);
+
+    //   this.startButton.addEventListener("click", this.start);
   }
 
-  start() {
-    console.log("start() function was called!");
+  start = () => {
+    console.log("clicou");
+    this.tick();
+    this.counter = setInterval(this.tick, 1000);
+  };
+  pause = () => {
+    clearInterval(this.counter);
+  };
+  onDurationChange = () => {};
+  tick = () => {
+    console.log("tick");
+    const timeRemaining = this.timeRemaining;
+    this.timeRemaining = timeRemaining - 1;
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+  set timeRemaining(time) {
+    this.durationInput.value = time;
   }
 }
 export default Timer;
--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ const startBtn = document.querySelector("#start");
 const pauseBtn = document.querySelector("#pause");
 
 const timer = new Timer(durationInput, startBtn, pauseBtn);
+startBtn.addEventListener("click", timer.start);
+pauseBtn.addEventListener("click", timer.pause);
